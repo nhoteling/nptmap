c7199e8b66116e216372a3c8c644350d66556200
nhoteling <nhoteling@gmail.com>
2021-05-14 13:51:29 -0400

    Add d3 vis

--- js/npt-map.js
+++ js/npt-map.js
@@ -1,8 +1,9 @@
 // Create SVG containers 
-var margin = {left:20, right:20, bottom:20, top:20};
-var container = div.append("div").style("position", "relative");
+var margin = {left:5, right:20, bottom:20, top:20};
+var container = div.append("div").attr("class","map-container");
 var svg1 = container 
     .append("svg")
+    .attr("class", "container1")
     .attr("id", "map")
     .attr("width", width)
     .attr("height", height);
@@ -12,20 +13,54 @@ var svg1 = container
 // Data
 var npt = data.npt;
 var adk = data.adk;
-
+var brd = data.brd;
 
 // Scales 
 var minLon = d3.min(adk, function(d) {return d.lon;});
 var maxLon = d3.max(adk, function(d) {return d.lon;});
-var minLat = d3.min(npt, function(d) {return d.lat;});
-var maxLat = d3.max(npt, function(d) {return d.lat;});
-var xScale = d3.scaleLinear().domain([minLon,maxLon]).range([margin.left,width-margin.right]);   // -76, -73
-var yScale = d3.scaleLinear().domain([minLat,maxLat]).range([height-margin.bottom,margin.top]);    //  43,45
+var minLat = d3.min(adk, function(d) {return d.lat;});
+var maxLat = d3.max(adk, function(d) {return d.lat;});
+var minVal = d3.min(adk, function(d) {return d.value;});
+var maxVal = d3.max(adk, function(d) {return d.value;});
+
+var xScale = d3.scaleLinear().domain([minLon,maxLon]).range([margin.left,width-margin.right]);  
+var yScale = d3.scaleLinear().domain([minLat,maxLat]).range([height-margin.bottom,margin.top]);    
+var eScale = d3.scaleLinear().domain([minVal,maxVal]).range([0,40]);
+
 
 var parseTime = d3.timeParse("%Y-%m-%d %H:%M:%S");
-// dates for parks data
 let timeData = [];
 npt.forEach(function(d) { timeData.push(parseTime(d.time)); });
+
+// Path constructors
+var line = d3.line()
+             .x(function(d) { return xScale(d.lon); })
+             .y(function(d) { return yScale(d.lat); });
+
+var eline = d3.area()
+              .defined(function(d) { return d.lon !== maxLon; })
+              .x(function(d) { return xScale(d.lon); })
+              .y0(yScale(minLat))
+              .y1(function(d) { return yScale(d.lat)-eScale(d.value); });
+
+
+// Elevation lines
+var adkpath = svg1.append("path")
+          .datum(data.adk)
+          .attr("class","adk-path")
+          .attr("d", eline);
+
+// NPT path
+var nptpath = svg1.append("path")
+          .datum(data.npt)
+          .attr("class", "npt-path")
+          .attr("d", line);
+
+// ADK Border
+var adkbrd = svg1.append("path")
+          .datum(data.brd)
+          .attr("class", "adk-border")
+          .attr("d", line);
 
 
 // Marker - set to transparent by default
@@ -38,9 +73,9 @@ var marker = svg1.selectAll("circle")
   .attr("cx", function(d) { return xScale(d.lon); })
   .attr("cy", function(d) { return yScale(d.lat); })
   .attr("r", 5)
-  .style("fill", "#4682b4")
   .attr("opacity", 0.0);
-
+  
+// Show the first point only
 svg1.selectAll("#trail-marker")
         .filter(function(d) { return parseTime(d.time).valueOf() == timeData[0].valueOf(); })
         .attr("opacity",1.0);
@@ -73,18 +108,8 @@ container.append('svg')
     .call(sliderSimple);
 
 
-// Path
-var line = d3.line()
-             .x(function(d) { return xScale(d.lon); })
-             .y(function(d) { return yScale(d.lat); });
 
-var nptpath = svg1.append("path")
-          .datum(data.npt)
-          .attr("class", "npt-path")
-          .attr("d", line)
-          .style("fill", "none")
-  				.style("stroke", "#4682b4")
-  				.style("stroke-width",1.0)
-  				.style("opacity",1.0);
+
+
 
 
